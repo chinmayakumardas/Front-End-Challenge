f1af624eae006bd321150a09918a6e5012152530
chinmayakumardas <chinmayakumardas2000@gmail.com>
2024-01-04 22:11:18 +0530

    add

---
+++ Javascript/Array/array.js
@@ -0,0 +1,43 @@
+//array
+let myarr=["ajay",23,"CDA SEC-2 "]
+
+
+let arr=[5,8,1,5,33,145];
+//console.log(arr.push(5));
+// console.log(arr.unshift(6));
+// console.log(arr.splice(1,0,5.5,6.6));
+
+//deletetion
+// arr.pop();
+// arr.shift();
+// arr.splice(1,2);
+//console.log(arr.length);
+
+
+
+//arr.length=0;
+ 
+//combine array
+let a =[1,2,2,3];
+let b=[5,8,654];
+let c=[...a,...b];
+console.log(c.length);
+
+let d =[...a]
+console.log(d.length);
+
+
+//itertion of array
+for(let val of arr){
+    console.log(val);
+}
+
+let e=a.concat(d);
+
+//slice in array
+
+
+
+
+
+
