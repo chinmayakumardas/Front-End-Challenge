d960deb23b566c64c3f21c6d31ef334b110639ab
chinmayakumardas <chinmayakumardas2000@gmail.com>
2024-01-04 20:06:42 +0530

    add

--- Javascript/Basic/date.js
+++ Javascript/Basic/date.js
@@ -1,2 +1,10 @@
 let date=new Date();
 console.log(date.toString())
+console.log(date.toDateString())
+console.log(date.toTimeString())
+console.log(date.toJSON())
+console.log(date.toISOString())
+console.log(date.toLocaleDateString())
+console.log(date.toLocaleString())
+console.log(date.toLocaleTimeString())
+
