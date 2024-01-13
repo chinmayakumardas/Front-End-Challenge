45c01b52e6ade987b9a25f543d1a7bbf1c81a1cb
chinmaya-kumar-das <https://github.com/chinmaya-kumar-das>
2024-01-13 21:26:57 +0530

    dcdc

--- Javascript/06_Projects/5-keyboard/chaiaurcode.js
+++ Javascript/06_Projects/5-keyboard/chaiaurcode.js
@@ -1,1 +1,23 @@
-console.log('Project 5');
+//console.log('Project 5');
+
+const insert=document.getElementById("insert");
+
+window.addEventListener('keydown',(e)=>{
+    insert.innerHTML=`
+        <div class='color'>
+        <table>
+        <tr>
+          <th>Key</th>
+          <th>Keycode</th>
+          <th>code</th>
+        </tr>
+        <tr>
+          <td>${e.key===""? "space":e.key}</td>
+          <td>${e.keyCode}</td>
+          <td>${e.code}</td>
+        </tr>
+       
+       </table>
+        </div>
+    `;
+});
