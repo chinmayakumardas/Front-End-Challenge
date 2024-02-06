90e11aa45e775353010ed85ecdb24373cebfbb60
chinmaya-kumar-das <chinmayakumardas2000@gmail.com>
2024-02-06 22:59:43 +0530

    add

--- Javascript/Practice/Practice_1.js
+++ Javascript/Practice/Practice_1.js
@@ -5,14 +5,30 @@
 
 // let var const
 
-var x=10;
-{
-  var  x=0;
-    console.log(x);
-}
+// var x=10;
+// {
+//   var  x=0;
+//     console.log(x);
+// }
 
-let n=0;
-{
-     n=4;
-    console.log(n);
-}
+// let n=0;
+// {
+//      n=4;
+//     console.log(n);
+// }
+//  const z="fuck"
+// console.log(z);{
+//      z=3;
+//     console.log(z);
+// }
+
+//datatypes in js
+
+let a=2;
+let age=" "
+let add;
+let xyz=[1,3,2,4,3]
+let name="Ajay"
+const eligible=false;
+console.log("Welcome to variable");
+console.table([a,age,add,name,xyz,eligible])
