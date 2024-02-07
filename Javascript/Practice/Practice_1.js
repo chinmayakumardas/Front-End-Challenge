c28ff810f090b30fd1936157f91179bbdb2832f6
chinmaya-kumar-das <chinmayakumardas2000@gmail.com>
2024-02-07 16:28:29 +0530

    add

--- Javascript/Practice/Practice_1.js
+++ Javascript/Practice/Practice_1.js
@@ -24,8 +24,8 @@
 
 //datatypes in js
 
-let a=2;
-let age=" "
+let ab=2;
+let age=" ";
 let add;
 let xyz=[1,3,2,4,3]
 let name="Ajay"
@@ -48,7 +48,7 @@ let symbol2= Symbol("symbol 1")
 
 console.log(symbolA==symbolB);
 console.log(symbol1==symbol2);
-console.table([a,age,add,name,xyz,eligible,objects])
+console.table([ab,age,add,name,xyz,eligible,objects])
 console.log(objects);
 console.log(objects.length);
 console.log(objects[0]);
@@ -71,3 +71,35 @@ console.log(objects2.contact.email);
 console.log(objects2.contact.Addrs);
     
 
+let a=3;
+let b=4;
+//arethmetic operator
+console.log(a,b);
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+
+//assignment
+console.log(a+=9);
+console.log(a==a);
+console.log(a>=0);
+console.log(a==true);
+//string
+let name1="chinmaya";
+let name2="chinmaya"
+console.log(name1==name2);
+console.log("................");
+let n=2020;
+let m="2020";
+console.log(m==n);
+console.log(m===n);
+console.log(m==n);
+console.log(1>2?"1 is big":"2 is big");
+
+let str1="chinmaya"
+let str2="kumar"
+console.log(str1+str2);
+console.log(str1+" "+str2);
+
+console.log("calculate"+2);
+console.log(2**10);
