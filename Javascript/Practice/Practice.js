ecc647e921f8df973fccaa6d2bc679c45b758395
chinmaya-kumar-das <chinmayakumardas2000@gmail.com>
2024-02-08 23:05:36 +0530

    add

--- Javascript/Practice/Practice.js
+++ Javascript/Practice/Practice.js
@@ -1,9 +1,10 @@
-let x=4;
-
-
-function vvv(x){
-    return console.log(x)
-}
-vvv(x);
-console.log(x+1)
-console.log(x)
+// function name(a, b) {
+//   return a + b;
+// }
+// let res = name(2, 4);
+// console.log(res);
+var a = 6;
+var b = 6;
+// a += b;
+var a= a+=b;
+console.log(a);
